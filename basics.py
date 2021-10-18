00b54d3aaa2ec8b10e2d13d752284f73be887861
Rachana <rachana100902@gmail.com>
2021-10-18 16:44:30 +0530

    Variables basics

--- basics.py
+++ basics.py
@@ -1,1 +1,22 @@
 print ("Hello world")
+
+############## VARIABLES ##############
+
+# Variables are declared by using the = operator. Data type need not to be mentioned along with. 
+num=20
+num2=12.45
+name="Rachana"
+c=23+4j
+print(name)
+print(type(num))
+print(type(num2))
+print(type(name))
+print(type(c))
+# type is used to know the type of data the variable contains.
+a,b=2.5,4  #multiple assigment
+print(a,b) #print value in same line
+del a #deletes a
+print (a) #gives error
+ 
+
+
