385261bdc5f4b157e0dd84bb1ed4cf26aa7596ab
Rachana <rachana100902@gmail.com>
2021-10-18 17:54:03 +0530

    bool and comparisons operators

---
+++ comparisons.py
@@ -0,0 +1,23 @@
+# And, Or, Not are used as it is . No special symbols are required. 
+# preference of not > and > or 
+a=True
+b=True
+c=False
+print(a and b) #True
+print(a or b)  #True
+print (a or c) #True
+print (a and c) #False
+print (not(a)) #False
+print(not (c)) #True
+print((a and b)and c) #false
+
+
+######### COMPARISONS ##########
+
+# Symbols   Task_Performed
+#    ==        True, if equal
+#    !=        True, if not equal
+#    >         Greater than
+#    <         Less than
+#    <=        Less than or equal to
+#    >=        Greater than or equal to
