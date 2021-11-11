0d0f978435008c6325398824e865b6adebc3bbe4
Rachana <rachana100902@gmail.com>
2021-11-11 23:44:30 +0530

    escape sequence as normal text added

--- escape_seq.py
+++ escape_seq.py
@@ -3,3 +3,7 @@ print("This is double backslash\\\\")
 print("This is \t tab")
 print("This is new line\n In new line")
 print("hell\blo") #\b will erase the previous character.
+
+print("\\n for new line") #double slash is used along to print it as it is not as a escape sequence.
+print("4 backslash \\\\\\\\")
+print("tab \\t")
