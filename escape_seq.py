3e571932feaa82c8932336d90208b5c504d91ae7
Rachana <rachana100902@gmail.com>
2021-11-11 23:37:49 +0530

    backspace \b added

--- escape_seq.py
+++ escape_seq.py
@@ -2,3 +2,4 @@ print("This is backslash\\")
 print("This is double backslash\\\\")
 print("This is \t tab")
 print("This is new line\n In new line")
+print("hell\blo") #\b will erase the previous character.
