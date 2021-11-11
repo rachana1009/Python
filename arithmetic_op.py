fbd74cb3bc118a0232036beeabd1ca80bcba7170
Rachana <rachana100902@gmail.com>
2021-11-12 00:29:05 +0530

    arithmetic opr=erators added

---
+++ arithmetic_op.py
@@ -0,0 +1,16 @@
+# + : to add
+# - : to subtract
+# * : to multiply
+# / : float divison
+# // : integer divsion  
+# ** : power 
+# % :modulus ( gives remainder )
+
+print(2*3)
+print(2+3)
+print(2-3)
+print(20/3)
+print(20//3)
+print(2**3)
+print(round(2**1.5,4))
+print(30%4)
