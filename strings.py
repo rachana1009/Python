7a11f5e186afb9e89997ef321ca586fe2c3b1d97
Rachana <rachana100902@gmail.com>
2021-11-12 12:00:15 +0530

    basics of strings added

---
+++ strings.py
@@ -0,0 +1,5 @@
+name="Rachana"
+
+#string indexing
+print(name[2]) #will print c that is alphabet at second index from beginning.
+print(name[-2]) # will print a that is second index alphabet from end.
