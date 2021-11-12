eb0b123e9f4ac8c507be026431866a9cbbacdcd2
Rachana <rachana100902@gmail.com>
2021-11-12 12:03:46 +0530

    bstring slicing added

--- strings.py
+++ strings.py
@@ -2,4 +2,7 @@ name="Rachana"
 
 #string indexing
 print(name[2]) #will print c that is alphabet at second index from beginning.
-print(name[-2]) # will print a that is second index alphabet from end.
+print(name[-2]) # will print n that is second index alphabet from end.
+
+#string slicing
+print(name[1:5]) #will print 'acha' as 5th alphabet will not be included.
