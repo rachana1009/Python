fe58bb68d7344868864f35fd69f0ec1fe68c7b40
Rachana <rachana100902@gmail.com>
2021-11-12 12:08:51 +0530

    examples of string slicing added

--- strings.py
+++ strings.py
@@ -6,3 +6,8 @@ print(name[-2]) # will print n that is second index alphabet from end.
 
 #string slicing
 print(name[1:5]) #will print 'acha' as 5th alphabet will not be included.
+print(name[0:-2])
+print(name[0:]) #Rachana will be printed
+print(name[:]) #Rachana will be printed
+print(name[4:])
+print(name[:-3])
