07cf4b7ceaa547fb13fb815c2ac25cc0967b381e
Rachana <rachana100902@gmail.com>
2021-11-13 15:07:19 +0530

    conditional statements added

---
+++ conditional_statements.py
@@ -0,0 +1,7 @@
+age=int(input("Enter your age:"))
+if(age >= 18):
+ print("You are elligble to vote and drive with a license")
+elif ( age >=16 ) and (age<18):
+    print("You are not elligble to vote but drive with a learning license")
+else:
+    print("You are not elligble to vote and drive")
