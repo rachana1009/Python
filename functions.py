22504e13f7f56b7947114007ec9905c7f80a73af
Rachana <rachana100902@gmail.com>
2021-11-13 16:53:52 +0530

    fibonacci func added

--- functions.py
+++ functions.py
@@ -5,3 +5,23 @@ print( add_two(2,9) )
 
 total=add_two(9,8)
 print(total)
+
+print( add_two("rohan","preet"))
+
+
+def fibonacci_seq(n):
+    a=0
+    b=1
+    if n==1:
+        print(a)
+    elif n==2:
+        print(a, b)
+    else:
+        print(a,b ,end=" ")
+        for i in range(n-2):
+            c=a+b
+            a=b
+            b=c
+            print(b ,end=" ")
+
+fibonacci_seq(7)
